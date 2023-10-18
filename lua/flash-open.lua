ae9d61a1fdf16d3b5ee2666359dfa68889f41058
xt <1014797089@qq.com>
2023-10-19 04:55:38 +0800

    feat: 增加在正常模式下打开当前目录

---
+++ lua/flash-open.lua
@@ -0,0 +1,23 @@
+local M = {}
+
+local function get_currentpwd()
+  local path = vim.fn.getcwd()
+  return path
+end
+
+function M.open()
+  local path = get_currentpwd()
+  if path ~= '' then
+    local cmd = string.format("open -R \"%s\"", path)
+    os.execute(cmd)
+    print('Path opened with Finder: ' .. path)
+  else
+    print('Clipboard is empty')
+  end
+end
+
+function M.setup()
+  vim.cmd('command! Openfinder :lua require(\'plugins.finder\').open()')
+end
+
+return M
